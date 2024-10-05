2c12c482b13d72c2f07b4f351582a8ce7f6b3d0f
josephwilms7 <josephwilmsdev@gmail.com>
2024-10-05 10:28:46 -0600

    Add Wind Chill and improve

--- scripts/place.js
+++ scripts/place.js
@@ -7,3 +7,26 @@ year.innerHTML = today.getFullYear();
 const lastModified = document.getElementById("lastModified");
 
 lastModified.innerHTML = document.lastModified;
+
+
+const temperature = 8;
+const windSpeed = 22;
+
+function calculateWindChill(temp, wind) {
+    return (13.12 + (0.6215 * temp) - (11.37 * Math.pow(wind, 0.16)) + (0.3965 * temp * Math.pow(wind, 0.16))).toFixed(1);
+}
+
+function displayWindChill() {
+    let windChillElement = document.querySelector('#wind_chill');
+    let windChill;
+
+    if (temperature <= 10 && windSpeed > 4.8) {
+        windChill = calculateWindChill(temperature, windSpeed);
+    } else {
+        windChill = "N/A";
+    }
+
+    windChillElement.textContent = windChill;
+}
+
+window.onload = displayWindChill;
